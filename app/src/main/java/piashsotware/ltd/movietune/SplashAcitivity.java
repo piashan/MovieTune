bb7344a47343f2ab21a3ef9a7ce16299ef6361ee
piash <piash599@gmail.com>
2016-11-24 21:33:00 +0600

    added splash screen for 2.5 seconds

---
+++ app/src/main/java/piashsotware/ltd/movietune/SplashAcitivity.java
@@ -0,0 +1,28 @@
+package piashsotware.ltd.movietune;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashAcitivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent openMainActivity =  new Intent(SplashAcitivity.this, MainActivity.class);
+                startActivity(openMainActivity);
+                finish();
+
+            }
+        }, 2500);
+
+    }
+}
