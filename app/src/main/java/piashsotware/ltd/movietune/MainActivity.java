b8ef791674a67b17ac3cca330001a382c8a5cb90
piash <piash599@gmail.com>
2016-11-24 21:50:14 +0600

    added Tab and background image

--- app/src/main/java/piashsotware/ltd/movietune/MainActivity.java
+++ app/src/main/java/piashsotware/ltd/movietune/MainActivity.java
@@ -1,15 +1,26 @@
 package piashsotware.ltd.movietune;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import piashsotware.ltd.movietune.adapter.TabFragmentPageAdapter;
+
 public class MainActivity extends AppCompatActivity {
 
+    private TabFragmentPageAdapter mAdapter;
+    private ViewPager mViewPager;
+    private TabLayout mTabLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mViewPager = (ViewPager) findViewById(R.id.viewpager);
+        mTabLayout = (TabLayout) findViewById(R.id.sliding_tabs);
 
-
+        mAdapter = new TabFragmentPageAdapter(getSupportFragmentManager(), getApplication());
+        mViewPager.setAdapter(mAdapter);
+        mTabLayout.setupWithViewPager(mViewPager);
     }
 }
---
+++ app/src/main/java/piashsotware/ltd/movietune/adapter/TabFragmentPageAdapter.java
@@ -0,0 +1,38 @@
+package piashsotware.ltd.movietune.adapter;
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+import piashsotware.ltd.movietune.MovieFragment;
+
+/**
+ * Created by piash on 11/24/16.
+ */
+
+public class TabFragmentPageAdapter extends FragmentStatePagerAdapter {
+
+    private Context mContext;
+    private String tabTitles[] = new String[] { "New Release", "Top Rated" ,"Upcomming"};
+
+    public TabFragmentPageAdapter(FragmentManager fm, Context context) {
+        super(fm);
+        this.mContext = context;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return new MovieFragment();
+    }
+
+    @Override
+    public int getCount() {
+        return tabTitles.length;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return tabTitles[position];
+    }
+}
